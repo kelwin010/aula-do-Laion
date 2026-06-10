3d445e8aa6281a2de577efa758995f71d61bf9f9
kelwin010 <167237438+kelwin010@users.noreply.github.com>
2026-06-10 14:39:04 +0000

    aula-sobre navegação em telas 10-06

--- teste-app/src/app/index.tsx
+++ teste-app/src/app/index.tsx
@@ -1,10 +1,18 @@
-import { Text, View, StyleSheet } from "react-native";
+import { router } from "expo-router";
+import { Text, View, StyleSheet, Touchable, TouchableOpacity } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
+  <View style={styles.container}>
+    <Text style={styles.titulo}> HOME</Text>
+    <Text style={styles.subtitulo}>Bem vindo ao nosso APP!</Text>
+    <TouchableOpacity
+    style={styles.Botao}
+    onPress={router.push('/destalhes')}
+  >
+    <Text style={styles.textoBotao}><Detalhes>></Text>
+    </TouchableOpacity>
+  </View>
   );
 }
 
